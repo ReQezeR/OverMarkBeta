c71a82821534e289ad6e140fff14b5e07c0c16ea
ReQezeR <mp62639@o2.pl>
2020-11-23 22:01:15 +0100

    update const Strings

--- lib/services/admob_provider.dart
+++ lib/services/admob_provider.dart
@@ -1,13 +1,12 @@
 
-import 'package:admob_flutter/admob_flutter.dart';
-import 'package:flutter/material.dart';
+const String testDevice = 'ca-app-pub-9818237769586714~9232332501';
 
 class AdMobStatic{
   static bool testing = true;
 
   static String appId = 'ca-app-pub-9818237769586714~9232332501';
 
-  static String realAdsBannerUnitId = 'ca-app-pub-';
+  static String realAdsBannerUnitId = 'ca-app-pub-9818237769586714/6888174539';
   static String realAdsInterstitialUnitId = 'ca-app-pub-9818237769586714/4276546962';
   static String realAdsNativeUnitId = 'ca-app-pub-9818237769586714/5979483200';
   static String realAdsRewardVideoUnitId = 'ca-app-pub-';
@@ -17,18 +16,11 @@ class AdMobStatic{
       testing ? 'ca-app-pub-3940256099942544/6300978111' : realAdsBannerUnitId;
 
   static String interstitialAdUnitId =
-      !testing ? 'ca-app-pub-3940256099942544/1033173712' : realAdsInterstitialUnitId;
+      testing ? 'ca-app-pub-3940256099942544/1033173712' : realAdsInterstitialUnitId;
 
   static String nativeAdUnitId =
       !testing ? 'ca-app-pub-3940256099942544/2247696110' : realAdsNativeUnitId;
 
   static String rewardVideoAdUnitId =
       testing ? 'ca-app-pub-3940256099942544/5224354917' : realAdsRewardVideoUnitId;
-
-  // uncomment if using Google Ads
-  // static const MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
-  //   testDevices: <String>[],
-  //   nonPersonalizedAds: true,
-  //   keywords: <String>['gaming', 'flutter'],
-  // );
 }
