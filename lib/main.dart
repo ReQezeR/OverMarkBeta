07b03c5fa5ad2d8200af5e323fb66bb63f23f6b7
ReQezeR <mp62639@o2.pl>
2020-12-11 16:18:58 +0100

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:admob_flutter/admob_flutter.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:OverMark/pages/main_page.dart';
 import 'package:OverMark/themes/dark_theme.dart';
@@ -6,19 +9,22 @@ import 'package:OverMark/themes/light_theme.dart';
 import 'package:flutter/services.dart';
 import 'package:theme_provider/theme_provider.dart';
 
-void main() {
+void main()async{
    SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.black,
     statusBarColor: Colors.black38,
   ));
+
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseApp defaultApp = await Firebase.initializeApp();
   Admob.initialize();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final FirebaseAnalytics analytics = FirebaseAnalytics();
   @override
   Widget build(BuildContext context) {
     return ThemeProvider(
@@ -36,6 +42,9 @@ class MyApp extends StatelessWidget {
         home: ThemeConsumer(
           child: MainPage(title: 'OverMark'),
         ),
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: analytics),
+        ],
       ),
     );
   }
